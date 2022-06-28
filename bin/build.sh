052662228daddfb3b4eec3ef0624c9ab49bfb9c1
Yu <349815610@qq.com>
2022-06-28 21:25:05 +0800

    fix build.sh v1.6.7

--- bin/build.sh
+++ bin/build.sh
@@ -10,6 +10,9 @@ rm -rf nacos-coredns-plugin
 git clone https://github.com/coredns/coredns.git
 git clone https://github.com/nacos-group/nacos-coredns-plugin.git
 
+# cd nacos-coredns-plugin directory
+cd $GOPATHgit /src/nacos-coredns-plugin
+git checkout -b v1.6.7 origin/v1.6.7
 # cd coredns directory
 cd $GOPATH/src/coredns
 git checkout -b v1.6.7 v1.6.7
@@ -21,8 +24,8 @@ cp -r ../nacos-coredns-plugin/forward/setup.go plugin/forward
 
 # insert nacos into plugin
 sed -i '/hosts/a\\t"nacos",' core/dnsserver/zdirectives.go
-sed -i '/coredns\/plugin\/hosts/a\\t_ "github.com/coredns/coredns/plugin/nacos"' core/plugin/zplugin.go
-sed -i '/hosts:hosts/a\\t_ nacos:nacos' plugin.cfg
+sed -i '/coredns\/plugin\/hosts/a\"coredns/plugin/nacos"' core/plugin/zplugin.go
+sed -i '/hosts:hosts/a\nacos:nacos' plugin.cfg
 
 # build
 make
