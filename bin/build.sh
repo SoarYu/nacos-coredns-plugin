2059841668d073221592648254bb0b40bbd96afa
SoarYu <51791348+SoarYu@users.noreply.github.com>
2022-07-05 16:13:08 +0800

    fix build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -8,11 +8,11 @@ rm -rf nacos-coredns-plugin
 
 # clone current codes
 git clone https://github.com/coredns/coredns.git
-git clone https://github.com/nacos-group/nacos-coredns-plugin.git
+git clone https://github.com/SoarYu/nacos-coredns-plugin.git
 
 # cd nacos-coredns-plugin directory
-cd $GOPATH/src/nacos-coredns-plugin
-git checkout -b v1.6.7 origin/v1.6.7
+#cd $GOPATH/src/nacos-coredns-plugin
+#git checkout -b v1.6.7 origin/v1.6.7
 # cd coredns directory
 cd $GOPATH/src/coredns
 git checkout -b v1.6.7 v1.6.7
@@ -21,10 +21,11 @@ go get github.com/cihub/seelog
 # copy nacos plugin to coredns
 cp -r ../nacos-coredns-plugin/nacos plugin/
 cp -r ../nacos-coredns-plugin/forward/setup.go plugin/forward
+cp -r ../nacos-coredns-plugin/conf conf
 
 # insert nacos into plugin
 sed -i '/hosts/a\\t"nacos",' core/dnsserver/zdirectives.go
-sed -i '/coredns\/plugin\/hosts/a\"coredns/plugin/nacos"' core/plugin/zplugin.go
+sed -i '/coredns\/plugin\/hosts/a\\t_ "coredns/plugin/nacos"' core/plugin/zplugin.go
 sed -i '/hosts:hosts/a\nacos:nacos' plugin.cfg
 
 # build
